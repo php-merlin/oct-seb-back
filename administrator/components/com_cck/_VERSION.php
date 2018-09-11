445839dddcc5a73716f353326d71c8054abc29d1
php-merlin <sweetheart94123@gmail.com>
2018-09-11 10:52:53 +0200

    Set Dev Status to "dev-4.0"

--- administrator/components/com_cck/_VERSION.php
+++ administrator/components/com_cck/_VERSION.php
@@ -17,7 +17,7 @@ final class JCckVersion extends JCckVersionObject
 	
 	public $DEV_LEVEL = '0';
 
-	public $DEV_STATUS = '';
+	public $DEV_STATUS = 'dev-4.0';
 
 	public $API_VERSION = array( 'v3'=>'3.13.0' );
 	
