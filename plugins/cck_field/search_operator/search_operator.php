5a2b6b85d3aaa36da0e8d7cc3041afd8080b64a4
php-merlin <sweetheart94123@gmail.com>
2018-09-18 19:16:02 +0200

    Include "search_operator" Field Plug-ins

---
+++ plugins/cck_field/search_operator/search_operator.php
@@ -0,0 +1,120 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				https://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2018 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_FieldSearch_Operator extends JCckPluginField
+{
+	protected static $type		=	'search_operator';
+	protected static $path;
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Construct
+	
+	// onCCK_FieldConstruct
+	public function onCCK_FieldConstruct( $type, &$data = array() )
+	{
+		if ( self::$type != $type ) {
+			return;
+		}
+		parent::g_onCCK_FieldConstruct( $data );
+	}
+	
+	// onCCK_FieldConstruct_SearchSearch
+	public static function onCCK_FieldConstruct_SearchSearch( &$field, $style, $data = array(), &$config = array() )
+	{
+		$data['label']			=	null;
+		$data['live']			=	null;
+		$data['markup']			=	null;
+		$data['markup_class']	=	null;
+		$data['validation']		=	null;
+		$data['variation']		=	null;
+		
+		if ( !isset( $config['construction']['match_mode'][self::$type] ) ) {
+			$data['match_mode']	=	array(
+										'none'=>JHtml::_( 'select.option', 'none', JText::_( 'COM_CCK_DISABLED' ) ),
+										''=>JHtml::_( 'select.option', '', JText::_( 'COM_CCK_ENABLED' ) )
+									);
+
+			$config['construction']['match_mode'][self::$type]	=	$data['match_mode'];
+		} else {
+			$data['match_mode']									=	$config['construction']['match_mode'][self::$type];
+		}
+
+		parent::onCCK_FieldConstruct_SearchSearch( $field, $style, $data, $config );
+	}
+
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+	
+	// onCCK_FieldPrepareContent
+	public function onCCK_FieldPrepareContent( &$field, $value = '', &$config = array() )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+	}
+	
+	// onCCK_FieldPrepareForm
+	public function onCCK_FieldPrepareForm( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		self::$path	=	parent::g_getPath( self::$type.'/' );
+		parent::g_onCCK_FieldPrepareForm( $field, $config );
+
+		$field->form	=	'';
+		$field->value	=	$field->defaultvalue;
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
+	}
+	
+	// onCCK_FieldPrepareSearch
+	public function onCCK_FieldPrepareSearch( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+		
+		// Prepare
+		self::onCCK_FieldPrepareForm( $field, $value, $config, $inherit, $return );
+		
+		// Return
+		if ( $return === true ) {
+			return $field;
+		}
+	}
+	
+	// onCCK_FieldPrepareStore
+	public function onCCK_FieldPrepareStore( &$field, $value = '', &$config = array(), $inherit = array(), $return = false )
+	{
+		if ( self::$type != $field->type ) {
+			return;
+		}
+	}
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Render
+	
+	// onCCK_FieldRenderContent
+	public static function onCCK_FieldRenderContent( $field, &$config = array() )
+	{
+		return parent::g_onCCK_FieldRenderContent( $field );
+	}
+	
+	// onCCK_FieldRenderForm
+	public static function onCCK_FieldRenderForm( $field, &$config = array() )
+	{
+		return parent::g_onCCK_FieldRenderForm( $field );
+	}
+}
+?>
---
+++ plugins/cck_field/search_operator/tmpl/edit.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				https://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2018 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+JCckDev::forceStorage();
+?>
+
+<div class="seblod">
+	<?php echo JCckDev::renderLegend( JText::_( 'COM_CCK_CONSTRUCTION' ), JText::_( 'PLG_CCK_FIELD_'.$this->item->type.'_DESC' ) ); ?>
+    <ul class="adminformlist adminformlist-2cols">
+        <?php
+        echo JCckDev::renderForm( 'core_search_operators', $this->item->defaultvalue, $config );
+		
+		echo JCckDev::renderSpacer( JText::_( 'COM_CCK_STORAGE' ), JText::_( 'COM_CCK_STORAGE_DESC' ) );
+		echo JCckDev::getForm( 'core_storage', $this->item->storage, $config );
+        ?>
+    </ul>
+</div>
+
+<?php
+/*
+Get some dev. fields packaged as well..
+JCckDev::renderForm( 'search_operator_open_toggle'
+JCckDev::renderForm( 'search_operator_open'
+JCckDev::renderForm( 'search_operator_and'
+JCckDev::renderForm( 'search_operator_or'
+JCckDev::renderForm( 'search_operator_close'
+JCckDev::renderForm( 'search_operator_close_toggle'
+*/
+?>
