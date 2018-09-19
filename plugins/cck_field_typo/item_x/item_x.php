0d8e7333b2511ec697b6e3f563733270d9ed74d4
php-merlin <sweetheart94123@gmail.com>
2018-09-19 17:32:15 +0200

    Include "item_x" Typography Plug-in

---
+++ plugins/cck_field_typo/item_x/item_x.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* @version 			SEBLOD 3.x More
+* @package			SEBLOD (App Builder & CCK) // SEBLOD nano (Form Builder)
+* @url				https://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2018 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Plugin
+class plgCCK_Field_TypoItem_X extends JCckPluginTypo
+{
+	protected static $type	=	'item_x';
+	
+	// -------- -------- -------- -------- -------- -------- -------- -------- // Prepare
+		
+	// onCCK_Field_TypoPrepareContent
+	public function onCCK_Field_TypoPrepareContent( &$field, $target = 'value', &$config = array() )
+	{		
+		if ( self::$type != $field->typo ) {
+			return;
+		}
+		
+		// Prepare
+		$typo	=	parent::g_getTypo( $field->typo_options );
+		$value	=	parent::g_hasLink( $field, $typo, $field->$target );
+		
+		// Set
+		if ( $field->typo_label ) {
+			$field->label	=	self::_typo( $typo, $field, $field->label, $config );
+		}
+		$field->typo		=	self::_typo( $typo, $field, $value, $config );
+	}
+	
+	// _typo
+	protected static function _typo( $typo, $field, $value, &$config = array() )
+	{
+		$app	=	JFactory::getApplication();
+		$pk		=	(int)$field->value;
+		$name	=	$app->input->getCmd( 'cck_item_x_referrer', $app->input->getCmd( 'referrer', uniqid() ) );
+
+		if ( !$pk ) {
+			return '';
+		}
+
+		return '<input type="hidden" id="'.$pk.'_'.$name.'" name="'.$name.'[]" value="'.$pk.'" data-cck-remove-before-search="" />';
+	}
+}
+?>
