dacbebfaa7fefc525f513a21413901f4c1084992
php-merlin <sweetheart94123@gmail.com>
2018-11-30 22:35:31 +0100

    Initial commit of Joomla! Language Restriction plug-in

---
+++ plugins/cck_field_restriction/joomla_language/install/script.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @version 			SEBLOD Multilingual 1.x
+* @package			SEBLOD Multilingual Add-on for SEBLOD 3.x
+* @url				https://www.seblod.com
+* @editor			Octopoos - www.octopoos.com
+* @copyright		Copyright (C) 2009 - 2018 SEBLOD. All Rights Reserved.
+* @license 			GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+defined( '_JEXEC' ) or die;
+
+// Script
+class plgCCK_Field_RestrictionJoomla_LanguageInstallerScript extends JCckInstallerScriptPlugin
+{
+}
+?>
