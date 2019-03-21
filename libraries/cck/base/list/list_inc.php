25baf3d352ef5182f4cefeb6b897c84c6729619b
php-merlin <sweetheart94123@gmail.com>
2019-03-21 19:44:20 +0100

    Fix conflicts (:@ !!)

--- libraries/cck/base/list/list_inc.php
+++ libraries/cck/base/list/list_inc.php
@@ -114,11 +114,7 @@ if ( ! $count ) {
 
 // Init
 $hasAjax		=	false;
-<<<<<<< HEAD
 $limitend		=	( isset( $preconfig['limitend'] ) && $preconfig['limitend'] != '' ) ? (int)$preconfig['limitend'] : (int)$options->get( 'pagination', JCck::getConfig_Param( 'pagination', 48 ) );
-=======
-$limitend		=	( isset( $preconfig['limitend'] ) && $preconfig['limitend'] != '' ) ? (int)$preconfig['limitend'] : (int)$options->get( 'pagination', JCck::getConfig_Param( 'pagination', 25 ) );
->>>>>>> master
 $list_context	=	'com_cck.'.$search->name;
 $pagination		=	( isset( $pagination ) && $pagination != '' ) ? $pagination : $options->get( 'show_pagination', 0 );
 
