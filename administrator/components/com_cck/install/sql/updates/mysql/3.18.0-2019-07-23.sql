98a66a844f2bef2038c06cad516935ce43c1ca97
php-merlin <sweetheart94123@gmail.com>
2019-11-07 16:25:24 +0100

    Update SQL files

---
+++ administrator/components/com_cck/install/sql/updates/mysql/3.18.0-2019-07-23.sql
@@ -0,0 +1,2 @@
+
+ALTER TABLE `#__cck_core_fields` ADD `language` CHAR(7) NOT NULL DEFAULT '*' AFTER `label`;
---
+++ administrator/components/com_cck/install/sql/updates/mysql/3.18.0-2019-11-07.sql
@@ -0,0 +1,2 @@
+
+ALTER TABLE `#__cck_core_sites` ADD `users` VARCHAR(255) NOT NULL AFTER `groups`;
