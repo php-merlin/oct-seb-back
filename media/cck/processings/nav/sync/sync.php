09616fc44a76a59a3ffc2d1cb0077e219bdd7271
php-merlin <sweetheart94123@gmail.com>
2020-03-20 09:21:29 +0100

    Sync Nav Lists

--- media/cck/processings/nav/sync/sync.php
+++ media/cck/processings/nav/sync/sync.php
@@ -1,82 +1,91 @@
 <?php
 defined( '_JEXEC' ) or die;
 
-if ( $context != 'com_menus.item' ) {
-	return;
-}
-if ( !$article->id ) {
+if ( !( isset( $article->id ) && $article->id ) ) {
 	return;
 }
 
-$item_request	=	array();
-$item_types		=	array(
-						'alias'=>true,
-						'heading'=>true,
-						'separator'=>true,
-						'url'=>true
-					);
+// Prepare
+if ( $context == 'com_menus.menu' ) {
+	$content		=	new JCckContentMenu;
+	$content_type	=	'o_nav_list';
+} elseif ( $context == 'com_menus.item' ) {
+	$item_request	=	array();
+	$item_types		=	array(
+							'alias'=>true,
+							'heading'=>true,
+							'separator'=>true,
+							'url'=>true
+						);
 
-if ( !isset( $item_types[$article->type] ) ) {
-	if ( $article->type == 'component' ) {
-		$request	=	explode( '&', $article->link );
+	if ( !isset( $item_types[$article->type] ) ) {
+		if ( $article->type == 'component' ) {
+			$request	=	explode( '&', $article->link );
 
-		if ( strpos( $article->link, '?option=com_cck&view=list' ) !== false ) {
-			foreach ( $request as $k=>$v ) {
-				if ( strpos( $v, 'search=' ) !== false || strpos( $v, 'task=' ) !== false ) {
-					$parts						=	explode( '=', $v );
-					$item_request[$parts[0]]	=	$parts[1];
+			if ( strpos( $article->link, '?option=com_cck&view=list' ) !== false ) {
+				foreach ( $request as $k=>$v ) {
+					if ( strpos( $v, 'search=' ) !== false || strpos( $v, 'task=' ) !== false ) {
+						$parts						=	explode( '=', $v );
+						$item_request[$parts[0]]	=	$parts[1];
+					}
 				}
-			}
-			$item_type	=	'com_cck.list';
-		} elseif ( strpos( $article->link, '?option=com_cck&view=form' ) !== false ) {
-			foreach ( $request as $k=>$v ) {
-				if ( strpos( $v, 'type=' ) !== false ) {
-					$parts						=	explode( '=', $v );
-					$item_request[$parts[0]]	=	$parts[1];
+				$item_type	=	'com_cck.list';
+			} elseif ( strpos( $article->link, '?option=com_cck&view=form' ) !== false ) {
+				foreach ( $request as $k=>$v ) {
+					if ( strpos( $v, 'type=' ) !== false ) {
+						$parts						=	explode( '=', $v );
+						$item_request[$parts[0]]	=	$parts[1];
+					}
 				}
-			}
-			$item_type	=	'com_cck.form';
-		} elseif ( strpos( $article->link, '?option=com_cck_toolbox&view=processing' ) !== false ) {
-			$item_type	=	'com_cck.custom';
-		} elseif ( strpos( $article->link, '?option=com_cck_webservices&view=api_docs' ) !== false ) {
-			$item_type	=	'com_cck.api-docs';
-		} elseif ( strpos( $article->link, '?option=com_cck_webservices&view=api' ) !== false ) {
-			$item_type	=	'com_cck.api';
-		} elseif ( strpos( $article->link, '?option=com_content&view=article' ) !== false ) {
-			foreach ( $request as $k=>$v ) {
-				if ( strpos( $v, 'id=' ) !== false ) {
-					$parts						=	explode( '=', $v );
-					$item_request[$parts[0]]	=	$parts[1];
+				$item_type	=	'com_cck.form';
+			} elseif ( strpos( $article->link, '?option=com_cck_toolbox&view=processing' ) !== false ) {
+				$item_type	=	'com_cck.custom';
+			} elseif ( strpos( $article->link, '?option=com_cck_webservices&view=api_docs' ) !== false ) {
+				$item_type	=	'com_cck.api-docs';
+			} elseif ( strpos( $article->link, '?option=com_cck_webservices&view=api' ) !== false ) {
+				$item_type	=	'com_cck.api';
+			} elseif ( strpos( $article->link, '?option=com_content&view=article' ) !== false ) {
+				foreach ( $request as $k=>$v ) {
+					if ( strpos( $v, 'id=' ) !== false ) {
+						$parts						=	explode( '=', $v );
+						$item_request[$parts[0]]	=	$parts[1];
+					}
 				}
-			}
-			$item_type	=	'com_content.article';
-		} elseif ( strpos( $article->link, '?option=com_content&view=category' ) !== false ) {
-			foreach ( $request as $k=>$v ) {
-				if ( strpos( $v, 'id=' ) !== false ) {
-					$parts						=	explode( '=', $v );
-					$item_request[$parts[0]]	=	$parts[1];
+				$item_type	=	'com_content.article';
+			} elseif ( strpos( $article->link, '?option=com_content&view=category' ) !== false ) {
+				foreach ( $request as $k=>$v ) {
+					if ( strpos( $v, 'id=' ) !== false ) {
+						$parts						=	explode( '=', $v );
+						$item_request[$parts[0]]	=	$parts[1];
+					}
 				}
+				$item_type	=	'com_content.category';
+			} else {
+				return;
 			}
-			$item_type	=	'com_content.category';
 		} else {
 			return;
 		}
 	} else {
-		return;
+		$item_type		=	$article->type;
 	}
+
+	$content		=	new JCckContentMenuItem;
+	$content_type	=	'o_nav_item';
 } else {
-	$item_type		=	$article->type;
+	return;
 }
 
-$canSave	=	false;
-$content	=	new JCckContentMenuItem;
+// Process
+$doMore	=	false;
 
 if ( $content->load( $article->id )->isSuccessful() ) {
-	$canSave	=	true;	
-} elseif ( $content->import( 'o_nav_item', $article->id )->isSuccessful() ) {
-	$canSave	=	true;
+	$doMore	=	true;
+} elseif ( $content->import( $content_type, $article->id )->isSuccessful() ) {
+	$doMore	=	true;
 }
-if ( $canSave ) {
+
+if ( $doMore && $content_type == 'o_nav_item' ) {
 	$content->setProperty( 'item_type', $item_type );
 	$content->setProperty( 'item_request', json_encode( $item_request ) );
 	$content->store();
