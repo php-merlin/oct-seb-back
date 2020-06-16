b41a6c1813a78b8b54a4d56fc53668b00ad4a89d
php-merlin <sweetheart94123@gmail.com>
2020-06-16 10:22:39 +0200

    Prepend PK field on Store
    Required for Free Child entries

--- libraries/cck/base/form/store_inc.php
+++ libraries/cck/base/form/store_inc.php
@@ -196,6 +196,16 @@ $session->clear( 'cck_hash_'.$unique.'_context' );
 // -------- -------- -------- -------- -------- -------- -------- -------- // Prepare Store
 
 if ( count( $fields ) ) {
+	if ( $type->storage_location ) {
+		$properties					=	array( 'key_field' );
+		$properties					=	JCck::callFunc( 'plgCCK_Storage_Location'.$type->storage_location, 'getStaticProperties', $properties );
+		$properties['key_field']	=	CCK_Form::getField( $properties['key_field'], $type->name );
+
+		if ( is_object( $properties['key_field'] ) ) {
+			array_unshift( $fields, $properties['key_field'] );
+		}
+	}
+
 	foreach ( $fields as $field ) {
 		$field->state	=	'';
 		$toBeChecked	=	false;
