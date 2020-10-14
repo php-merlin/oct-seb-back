683dd8f15c5861a435c624b6d0b2af02b5514d05
php-merlin <sweetheart94123@gmail.com>
2020-10-14 15:16:14 +0200

    Quick change

--- components/com_cck/layouts/cck/markup/wrapper/uk_dropdown.php
+++ components/com_cck/layouts/cck/markup/wrapper/uk_dropdown.php
@@ -3,6 +3,8 @@ defined( '_JEXEC' ) or die;
 
 if ( $displayData['parent']->getValue( 'o_nav_item_output_settings' ) ) {
 	$settings	=	$displayData['parent']->getValue( 'o_nav_item_output_settings' );
+} else {
+	$settings	=	'pos: bottom-justify;animation: uk-animation-slide-top-small; duration: 300';
 }
 ?>
 <div class="uk-dropdown" uk-dropdown="<?php echo $settings; ?>">
